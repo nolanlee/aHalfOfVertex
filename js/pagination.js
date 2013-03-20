c8c3abc54298ded79ee51d5aba648c5484f55150
nolanlee <nosaiforever@gmail.com>
2013-03-21 00:19:30 +0800

    continue do pagination

--- js/pagination.js
+++ js/pagination.js
@@ -1,14 +1,19 @@
-(function (window, $) {
+(function(window, $) {
 	var LOAD_OFFSET = 140,
 		PAGE_LENGTH = 15;
-		NEWS_URL = "../json/home.json";
-		
+	NEWS_URL = "../json/home.json";
+
 	var loadIndex = 1,
 		$content = $("#content"),
-		$loadingImg = $("#loadingImg"),
-		$pagination = $("#pagination");
+		$newsList = $("#news-list"),
+		$loadingImg = $("#loading-img"),
+		$pagination = $("#pagination"),
+		$previous = $("#previous"),
+		$next = $("#next"),
+		$go = $("#go"),
+		$pageNum = $("#page-num");
 
-	var sewNews = function (date, collect, like, topic, content) {
+	var sewNews = function(date, collect, like, topic, content) {
 		return '<div class="news-wrap"><div class="time-comment">' +
 			'<div><span class="news-date comment">' + date + '</span>' +
 			'<a class="comment-icon news-date-image" href="javascript:"></a></div>' +
@@ -20,86 +25,90 @@
 			'<div class="news-content">' + content + '</div></div></div>';
 	};
 
-	var renderLoadingImg = function () {
+	var displayLoadingImg = function() {
 		$loadingImg.css("display", "block");
 	};
 
-	/*
-	 *unfinished
-	 */
-	var renderPagination = function (pageNum, pageTotal) {
+	var disappearedLoadingImg = function() {
+		$loadingImg.css("display", "none");
+	};
+
+	var switchBreak = function(pageNum) {
+
+	};
+
+	var renderPagination = function(pageTotal, partLength) {
 		var pResult,
-			pPlaceholder = '...'
-			pRange = 5,
-			ptPart1='<li><a class="page-num" href="javascript:">',
+			frontPageTotal = Math.ceil(pageTotal / partLength),
+			pRange = frontPageTotal < 7 ? (frontPageTotal - 2) : 5,
+			ptPart1 = '<li><a class="page-num" href="javascript:">',
 			ptPart2 = '</a></li>',
-			ptHeader = '<ul id="pagination"><li><a id="previous" class="pagination-button" href="javascript:">' +
-				'FRONT</a></li><li><a class="page-num" href="javascript:">1</a></li>',
-			ptFooter = '<li><a class="page-num" href="javascript:">' + pageTotal + '</a></li>' +
-				'<li><a id="next" class="pagination-button" href="javascript:">NEXT</a></li>' +
-				'<li><input id="pageNum" type="text"/></li>' +
-				'<li><a id="go" class="pagination-button" href="javascript:">GO</a></li>';
-			
-			pResult = ptPart1 + 1 + ptPart2;
-			
-			for(var i = 0; i< pRange; i++) {
-				
-			}
-			
-			pResult += ptPart1 + pageTotal + ptPart2;
+			ptHeader = '<div id="pagination-wrap"><ul id="pagination"><li><a id="previous" class="pagination-button button-disabled" href="javascript:">FRONT</a></li><li><a class="page-num page-num-selected" href="javascript:">1</a></li><li><span id="pre-break" class="break-hidden">......<span></li>',
+			ptFooter = '<li><span id="next-break" class="break-hidden">......<span></li><li><a class="page-num" href="javascript:">' + frontPageTotal + '</a></li><li><a id="next" class="pagination-button button-enabled" href="javascript:">NEXT</a></li><li><input id="page-num" type="text"/></li><li><a id="go" class="pagination-button button-enabled" href="javascript:">GO</a></li></ul></div>';
+
+		pResult = ptHeader;
+
+		for (var i = 0; i < pRange; i++) {
+			pResult += ptPart1 + (i + 2) + ptPart2;
+		}
+
+		pResult += ptFooter;
 		return pResult;
 	};
 
-	var bindScroll = function () {
-		$(document).bind("scroll", loadNext);
+	var getNews = function(handler) {
+		$.get(NEWS_URL, loadIndex, function(data) {
+			handler(data);
+		});
 	};
 
-	var unbindScroll = function () {
-		$(document).unbind("scroll", loadNext);
-	}
-	
-	/*
-	 *unfinished
-	 */
-	var renderNews = function () {
+	var renderNews = function(data) {
 		if ($(".news-wrap").length >= PAGE_LENGTH) {
 			loadIndex = 1;
-			$.get();
+			unbindScroll();
+			$content.append(renderPagination(data.pageTotal, data.partLength));
 		} else {
 			loadIndex++;
-			renderLoadingImg();
-			$.get(NEWS_URL, loadIndex, function (data) {
-				var result = "";
-				
-				if (typeof data != "object") {
-					data = JSON.parse(data);
-				} 
-				
-				if(data !=null) {
-					$.each(data.news, function (index, news) {
-						result += sewNews(news.date, news.collect, news.like, news.topic, news.content);
-					});
-	
-					$content.append(result);
-					bindScroll();
-				}
+			displayLoadingImg();
 
-				$loadingImg.css("display", "none");
-			});
+			var result = "";
+
+			if (typeof data != "object") {
+				data = JSON.parse(data);
+			}
+
+			if (data != null) {
+				$.each(data.news, function(index, news) {
+					result += sewNews(news.date, news.collect, news.like, news.topic, news.content);
+				});
+
+				$newsList.append(result);
+				bindScroll();
+			}
+
+			disappearedLoadingImg();
 		}
 	};
 
-	var loadNext = function () {
+	var loadNext = function() {
 		var windowHeight = $(window).height() + $(window).scrollTop(),
 			loadHeight = $(document).height() - LOAD_OFFSET;
 
 		if (loadHeight <= windowHeight) {
 			unbindScroll();
-			renderNews();
+			getNews(renderNews);
 		}
 	};
 
-	var init = function () {
+	var bindScroll = function() {
+		$(document).bind("scroll", loadNext);
+	};
+
+	var unbindScroll = function() {
+		$(document).unbind("scroll", loadNext);
+	};
+
+	var init = function() {
 		bindScroll();
 	};
 
