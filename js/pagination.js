3d02f8553cf06c3725fb7f38491ae030600668bc
Nolan Li <nosaiforever@gmail.com>
2013-03-21 17:38:21 +0800

    refine pagination

--- js/pagination.js
+++ js/pagination.js
@@ -1,19 +1,17 @@
-(function(window, $) {
+(function (window, $) {
 	var LOAD_OFFSET = 140,
-		PAGE_LENGTH = 15;
-	NEWS_URL = "../json/home.json";
+		PAGE_LENGTH = 15,
+		PAGINATION_RANGE = 5,
+		NEWS_URL = "../json/home.json";
 
-	var loadIndex = 1,
+	var paginationElements = {},
 		$content = $("#content"),
 		$newsList = $("#news-list"),
 		$loadingImg = $("#loading-img"),
-		$pagination = $("#pagination"),
-		$previous = $("#previous"),
-		$next = $("#next"),
-		$go = $("#go"),
-		$pageNum = $("#page-num");
+		loadIndex = 1,
+		currentPageNum = 1;
 
-	var sewNews = function(date, collect, like, topic, content) {
+	var sewNews = function (date, collect, like, topic, content) {
 		return '<div class="news-wrap"><div class="time-comment">' +
 			'<div><span class="news-date comment">' + date + '</span>' +
 			'<a class="comment-icon news-date-image" href="javascript:"></a></div>' +
@@ -25,48 +23,119 @@
 			'<div class="news-content">' + content + '</div></div></div>';
 	};
 
-	var displayLoadingImg = function() {
+	var displayLoadingImg = function () {
 		$loadingImg.css("display", "block");
 	};
 
-	var disappearedLoadingImg = function() {
+	var disappearedLoadingImg = function () {
 		$loadingImg.css("display", "none");
 	};
 
-	var switchBreak = function(pageNum) {
-		
-	};
-
-	var renderPagination = function(pageTotal, partLength) {
+	var renderPagination = function (pageNum, pageTotal, partLength) {
 		var pResult,
+			pFloatBegin,
 			frontPageTotal = Math.ceil(pageTotal / partLength),
-			pRange = frontPageTotal < 7 ? (frontPageTotal - 2) : 5,
+			pRange = frontPageTotal < (PAGINATION_RANGE + 2) ? (frontPageTotal - 2) : PAGINATION_RANGE,
+			preBreak = '<li><span id="pre-break">......<span></li>',
+			nextBreak = '<li><span id="next-break">......<span></li>',
 			ptPart1 = '<li><a class="page-num" href="javascript:">',
 			ptPart2 = '</a></li>',
-			ptHeader = '<div id="pagination-wrap"><ul id="pagination"><li><a id="previous" class="pagination-button button-disabled" href="javascript:">FRONT</a></li><li><a class="page-num page-num-selected" href="javascript:">1</a></li><li><span id="pre-break" class="break-hidden">......<span></li>',
-			ptFooter = '<li><span id="next-break" class="break-hidden">......<span></li><li><a class="page-num" href="javascript:">' + frontPageTotal + '</a></li><li><a id="next" class="pagination-button button-enabled" href="javascript:">NEXT</a></li><li><input id="page-num" type="text"/></li><li><a id="go" class="pagination-button button-enabled" href="javascript:">GO</a></li></ul></div>';
+			ptHeader = '<div id="pagination-wrap"><ul id="pagination"><li><a id="previous" class="pagination-button button-disabled" href="javascript:">FRONT</a></li><li><a class="page-num" href="javascript:">1</a></li>',
+			ptFooter = '<li><a class="page-num" href="javascript:">' + frontPageTotal + '</a></li><li><a id="next" class="pagination-button button-enabled" href="javascript:">NEXT</a></li><li><input id="page-num" type="text"/></li><li><a id="go" class="pagination-button button-enabled" href="javascript:">GO</a></li></ul></div>';
+
+		var floatBeginFactory = function (floatBegain) {
+			if ((pageNum - 2) > 1 && (pageNum + 2) < frontPageTotal) {
+				floatBegain = pageNum - 2;
+			} else if ((pageNum - 2) <= 1 && (pageNum + 2) < frontPageTotal) {
+				floatBegain = 2;
+			} else {
+				floatBegain = frontPageTotal - PAGINATION_RANGE;
+			}
+			return floatBegain;
+		};
+
+		var switchBreak = function (pageNum, frontPageTotal) {
+			var rangeRadius = Math.ceil((PAGINATION_RANGE + 1) / 2);
+
+			if (pageNum > rangeRadius + 1) {
+				ptHeader = ptHeader + preBreak;
+			}
+
+			if (pageNum < frontPageTotal - PAGINATION_RANGE + rangeRadius - 1) {
+				ptFooter = nextBreak + ptFooter;
+			}
+		};
+
+		pFloatBegin = floatBeginFactory(pFloatBegin);
+		switchBreak(pageNum, frontPageTotal);
 
 		pResult = ptHeader;
 
 		for (var i = 0; i < pRange; i++) {
-			pResult += ptPart1 + (i + 2) + ptPart2;
+			pResult += ptPart1 + (pFloatBegin + i) + ptPart2;
 		}
 
 		pResult += ptFooter;
 		return pResult;
 	};
 
-	var getNews = function(handler) {
-		$.get(NEWS_URL, loadIndex, function(data) {
-			handler(data);
-		});
+	var initPaginitionElements = function () {
+		paginationElements.$pagination = $("#pagination-wrap"),
+		paginationElements.$previous = $("#previous"),
+		paginationElements.$next = $("#next"),
+		paginationElements.$go = $("#go"),
+		paginationElements.$pageNumInput = $("#page-num");
+		paginationElements.$pageNums = $(".page-num");
 	};
 
-	var renderNews = function(data) {
+	var doPagination = function () {
+		$newsList.empty();
+		paginationElements.$pagination.remove();
+		pagination = {};
+		getNews(renderNews);
+		bindScroll();
+	};
+
+	var initPaginationAction = function () {
+		paginationElements.$pageNums.on("click", function () {
+			currentPageNum = parseInt($(this).html());
+			doPagination();
+		});
+
+		paginationElements.$previous.on("click", function () {
+			if (currentPageNum > 1) {
+				currentPageNum--;
+				doPagination();
+			}
+		});
+
+		paginationElements.$next.on("click", function () {
+			if (currentPageNum < 1) {
+				currentPageNum++;
+				doPagination();
+			}
+		});
+
+		paginationElements.$go.on("click", function(){
+			var _pageNum = paginationElements.$pageNumInput.val();
+
+			if(_pageNum && _pageNum.match("^[0-9]*[1-9][0-9]*$")) {
+				currentPageNum = parseInt(_pageNum);
+				doPagination();
+			} else {
+				alert("请输入合法的页码:)");
+			}
+		});
+
+	};
+
+	var renderNews = function (data) {
 		if ($(".news-wrap").length >= PAGE_LENGTH) {
 			loadIndex = 1;
 			unbindScroll();
-			$content.append(renderPagination(data.pageTotal, data.partLength));
+			$content.append(renderPagination(currentPageNum, data.pageTotal, data.partLength));
+			initPaginitionElements();
+			initPaginationAction();
 		} else {
 			loadIndex++;
 			displayLoadingImg();
@@ -78,7 +147,7 @@
 			}
 
 			if (data != null) {
-				$.each(data.news, function(index, news) {
+				$.each(data.news, function (index, news) {
 					result += sewNews(news.date, news.collect, news.like, news.topic, news.content);
 				});
 
@@ -90,7 +159,13 @@
 		}
 	};
 
-	var loadNext = function() {
+	var getNews = function (handler) {
+		$.get(NEWS_URL, loadIndex, function(data){
+			handler(data);
+		});
+	};
+
+	var loadNext = function () {
 		var windowHeight = $(window).height() + $(window).scrollTop(),
 			loadHeight = $(document).height() - LOAD_OFFSET;
 
@@ -100,15 +175,16 @@
 		}
 	};
 
-	var bindScroll = function() {
+	var bindScroll = function () {
 		$(document).bind("scroll", loadNext);
 	};
 
-	var unbindScroll = function() {
+	var unbindScroll = function () {
 		$(document).unbind("scroll", loadNext);
 	};
 
-	var init = function() {
+	var init = function () {
+		getNews(renderNews);
 		bindScroll();
 	};
 
